31d6ac874deecfe997566d8d3a83d75fe25fecec
matthew harwood <matthhar12@gmail.com>
2017-02-12 21:24:35 -0800

    Updated to add store subdomains
    Need to add deployment.
    Need to add flags back in for port.
    Need to add ssl to store.

--- src/main/main.go
+++ src/main/main.go
@@ -1,33 +1,80 @@
 package main
 
 import (
-	"fmt"
-	"flag"
 	"net/http"
 	"github.com/labstack/echo"
 	"github.com/matthewharwood/morningharwood-server/src/api"
 	"github.com/labstack/echo/middleware"
+	//"flag"
 )
 
 func todoistEndpoint(c echo.Context) error {
 	return c.JSON(http.StatusOK, api.Todoist())
 }
 
+type (
+	Host struct {
+		Echo *echo.Echo
+	}
+)
 
 func main() {
-	port := flag.String("port", ":8000", "server port")
-	fmt.Println("start echo")
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Static("/", "morningharwood-client/assets/images")
-	e.File("/favicon.ico", "morningharwood-client/assets/images/favicon/favicon.ico")
-	e.File("/favicon-16x16.png", "morningharwood-client/assets/images/favicon/favicon-16x16.png")
-	e.File("/favicon-32x32.png", "morningharwood-client/assets/images/favicon/favicon-32x32.png")
-	e.File("/", "morningharwood-client/index.html")
+	//port := flag.String("port", ":8000", "server port")
+	// Hosts
+	hosts := make(map[string]*Host)
 
-	g := e.Group("/api/v1")
+
+	//------
+	// store
+	//------
+
+	store := echo.New()
+	store.Use(middleware.Logger())
+	store.Use(middleware.Recover())
+
+	hosts["store.localhost:1323"] = &Host{store}
+
+	store.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "store")
+	})
+
+	//---------
+	// Website
+	//---------
+
+	site := echo.New()
+	site.Static("/", "morningharwood-client/assets/images")
+	site.File("/favicon.ico", "morningharwood-client/assets/images/favicon/favicon.ico")
+	site.File("/favicon-16x16.png", "morningharwood-client/assets/images/favicon/favicon-16x16.png")
+	site.File("/favicon-32x32.png", "morningharwood-client/assets/images/favicon/favicon-32x32.png")
+	site.File("/", "morningharwood-client/index.html")
+	site.Use(middleware.Logger())
+	site.Use(middleware.Recover())
+
+	hosts["localhost:1323"] = &Host{site}
+
+
+	//-----
+	// API
+	//-----
+	g := site.Group("/api/v1")
 	g.Use(middleware.CORS())
 	g.GET("/todoist", todoistEndpoint)
-	e.Logger.Fatal(e.Start(*port))
+
+	// Server
+	e := echo.New()
+	e.Any("/*", func(c echo.Context) (err error) {
+		req := c.Request()
+		res := c.Response()
+		host := hosts[req.Host]
+
+		if host == nil {
+			err = echo.ErrNotFound
+		} else {
+			host.Echo.ServeHTTP(res, req)
+		}
+
+		return
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
